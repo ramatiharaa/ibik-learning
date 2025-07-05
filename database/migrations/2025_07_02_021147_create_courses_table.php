66cc789634c5b4c4da8b6c5d9576d3011ffb0531
ramatiharaa <ramatihara@gmail.com>
2025-07-05 22:23:10 +0700

    first commit

---
+++ database/migrations/2025_07_02_021147_create_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('category');
+            $table->string('lessons');
+            $table->string('instructor');
+            $table->string('role');
+            $table->string('students');
+            $table->unsignedTinyInteger('rating');
+            $table->string('image');
+            $table->string('avatar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
